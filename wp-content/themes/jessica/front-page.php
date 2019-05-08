60eb516ae7b5e9719ebc6253665f41b49fce5986
Dylan James Wagner <hello@dylanjameswagner.com>
2019-05-07 20:26:31 -0400

    spaces to tabs

--- wp-content/themes/jessica/front-page.php
+++ wp-content/themes/jessica/front-page.php
@@ -1,32 +1,32 @@
 <?php get_header(); ?>
 <main class="content">
-    <?php
-    $hero_image = get_field('hero_image');
-    $about_biography = get_field('about_biography');
-    $front_gallery = get_field('front_gallery');
-    ?>
+	<?php
+	$hero_image = get_field('hero_image');
+	$about_biography = get_field('about_biography');
+	$front_gallery = get_field('front_gallery');
+	?>
 
-    <div class="home-image" style="background-image: url(<?php echo wp_get_attachment_image_url($hero_image['id'], 'large'); ?>);">some shit</div>
+	<div class="home-image" style="background-image: url(<?php echo wp_get_attachment_image_url($hero_image['id'], 'large'); ?>);">some shit</div>
 
-    <?php if ($about_biography): ?>
-        <div class="about">
-            <?php echo $about_biography; ?>
-        </div>
-    <?php endif; ?>
+	<?php if ($about_biography): ?>
+		<div class="about">
+			<?php echo $about_biography; ?>
+		</div>
+	<?php endif; ?>
 
-    <?php if ($front_gallery): ?>
-        <section class="gallery">
-            <ul class="gallery__list">
-                <?php foreach ($front_gallery as $front_gallery_key => $front_gallery_value): ?>
-                    <li class="gallery__list-item">
-                        <?php echo wp_get_attachment_image($front_gallery_value['id'], 'large'); ?>
-                        <?php echo apply_filters('the_content', wp_get_attachment_caption($front_gallery_value['id'])); ?>
-                    </li>
-                <?php endforeach; ?>
-            </ul>
-        </section>
-    <?php endif; ?>
-    <?php
-    ?>
+	<?php if ($front_gallery): ?>
+		<section class="gallery">
+			<ul class="gallery__list">
+				<?php foreach ($front_gallery as $front_gallery_key => $front_gallery_value): ?>
+					<li class="gallery__list-item">
+						<?php echo wp_get_attachment_image($front_gallery_value['id'], 'large'); ?>
+						<?php echo apply_filters('the_content', wp_get_attachment_caption($front_gallery_value['id'])); ?>
+					</li>
+				<?php endforeach; ?>
+			</ul>
+		</section>
+	<?php endif; ?>
+	<?php
+	?>
 </main>
 <?php get_footer();
