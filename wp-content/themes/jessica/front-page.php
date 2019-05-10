5e71a01edccd92c8d38511bdf20c42d958a445bb
jessicalwilson <jessica.wilson@teamcolab.com>
2019-05-10 09:50:05 -0400

    Updates

--- wp-content/themes/jessica/front-page.php
+++ wp-content/themes/jessica/front-page.php
@@ -6,23 +6,13 @@
 	$front_gallery = get_field('front_gallery');
 	?>
 
-<<<<<<< HEAD
-    <div class="home-image" style="background-image: url(<?php echo wp_get_attachment_image_url($hero_image['id'], 'large'); ?>);"></div>
-    <h2>About</h2>
-    <?php if ($about_biography): ?>
-        <div class="about">
-            <?php echo $about_biography; ?>
-        </div>
-    <?php endif; ?>
-=======
-	<div class="home-image" style="background-image: url(<?php echo wp_get_attachment_image_url($hero_image['id'], 'large'); ?>);">some shit</div>
-
+	<div class="home-image" style="background-image: url(<?php echo wp_get_attachment_image_url($hero_image['id'], 'large'); ?>);"></div>
+	<h2>About</h2>
 	<?php if ($about_biography): ?>
 		<div class="about">
 			<?php echo $about_biography; ?>
 		</div>
 	<?php endif; ?>
->>>>>>> a7361c048b1b49b936d5ee852b0c03d4eda2a88c
 
 	<?php if ($front_gallery): ?>
 		<section class="gallery">
